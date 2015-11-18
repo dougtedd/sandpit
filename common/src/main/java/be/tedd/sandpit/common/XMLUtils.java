946ad015ac168681ebaf85dcd4a59a2b50ab01eb
Doug Tedd <doug@tedd.be>
2015-11-18 22:16:46 +0100

    dev

---
+++ common/src/main/java/be/tedd/sandpit/common/XMLUtils.java
@@ -0,0 +1,24 @@
+package be.tedd.sandpit.common;
+
+import org.w3c.dom.Document;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+
+public class XMLUtils {
+
+    public static String toString(Document doc) throws TransformerConfigurationException, TransformerException{
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        StreamResult result = new StreamResult(new StringWriter());
+        DOMSource source = new DOMSource(doc);
+        transformer.transform(source, result);
+        return result.getWriter().toString();
+    }
+}
--- jaxb/src/test/java/be/tedd/sandpit/jaxb/JaxbTest.java
+++ jaxb/src/test/java/be/tedd/sandpit/jaxb/JaxbTest.java
@@ -1,20 +1,43 @@
 package be.tedd.sandpit.jaxb;
 
 
+import be.tedd.sandpit.jaxb.shiporder.ObjectFactory;
+import be.tedd.sandpit.jaxb.shiporder.Shiporder;
 import org.junit.Assert;
 import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 
 
 /**
  */
 public class JaxbTest {
 
-
     @Test
-    public void testCreate() {
-        SimpleBean simpleBean = new SimpleBean();
-        Assert.assertNotNull(simpleBean);
-        Assert.assertNull(simpleBean.getStr());
+    public void testMarshall() throws Exception {
+        // Build an order
+        ObjectFactory factory = new ObjectFactory();
+        Shiporder shiporder = factory.createShiporder();
+        shiporder.setOrderid("ORDER-ID");
+
+        // Marshall the object
+        JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        StringWriter sw = new StringWriter();
+        jaxbMarshaller.marshal(shiporder, sw);
+
+        // Verify the output
+        String xmlString = sw.toString();
+        System.out.println(xmlString);
+        Assert.assertTrue(xmlString.contains("orderid=\"ORDER-ID\""));
     }
 
 }
